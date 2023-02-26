fe3523b7c162c374598e988959e3a8e70b7fc7cf
Albert Maršík <albert.marsik@gmail.com>
2023-02-26 23:49:58 +0100

    2player draw&play

--- app.js
+++ app.js
@@ -1,34 +1,60 @@
 import Card from "./card.js"
-export const PACK_PROPS = {top: 33.05, left: 47, height: 13.9, width: 10}
 export const TABLE_PROPS = {width: 80}
+export const PACK_PROPS = {top: 33.05, left: TABLE_PROPS.width*0.55, height: 13.9*0.6, width: 10*0.6}
 export let hand = []
+export let playedCards = []
+const NUMBER_OR_PLAYERS = 2
+export let player = 0
+let cardsLeft = 32
 
 //todo      refactor style to css and only attribut class
 //todo      limit rotation - issue -> communication between calculateleft and calculate rotation?
 //todo      opponent card drawing
 //todo      playing cards (incl recalculating positions of cards)
 
+for (let i=0; i<NUMBER_OR_PLAYERS; i++){
+    hand.push([])
+}
+
+
 window.addEventListener("DOMContentLoaded", ()=>{
+   
     const container = document.querySelector('#container')  
-    container.style.width = TABLE_PROPS.width + "vmin"     
+    container.style.width = TABLE_PROPS.width + "vw"     
 
     const pack = new Card(PACK_PROPS)
     pack.add()
-    
-    pack.card.style.backgroundColor = "red"
+    pack.card.textContent = "lízni si"
+    //pack.card.style.backgroundColor = "red"
 
     pack.card.addEventListener("mousedown", handlePackage)
     pack.card.addEventListener("touchstart", handlePackage)
 
     function handlePackage(){
+        player = (player+1)%NUMBER_OR_PLAYERS
         const card = new Card(PACK_PROPS)
-        hand.push(card)        
+        hand[player].push(card)        
         card.add()
+        card.card.textContent = "zahraj mě"
         requestAnimationFrame(()=>{
-            hand.forEach((currentCard, index)=>{
+            hand[player].forEach((currentCard, index)=>{
                 currentCard.move(currentCard, index)
             })
         }) 
+        cardsLeft--
+        if(!cardsLeft){
+            container.removeChild(pack.card)
+        }
+
+        card.card.addEventListener("mousedown", ()=>{
+            card.play()
+            playedCards.push(card)
+
+            const index = hand[card.player].indexOf(card)
+            hand[card.player].splice(index,1)
+            console.log("player: "+card.player)
+            console.log(hand[card.player])
+        })
     }
 
  
--- card.js
+++ card.js
@@ -1,35 +1,42 @@
-import { hand, PACK_PROPS, TABLE_PROPS } from "./app.js"
+import { hand, PACK_PROPS, player, TABLE_PROPS } from "./app.js"
 
 export default class Card {
     constructor(PACK_PROPS){
         // put in css and just assign class
         this.card = document.createElement("div") 
-        this.card.style.transition = "left 0.5s ease-in-out, top 0.5s ease-in-out, transform 0.5s ease-in-out"
+        this.card.classList.add("card")
         this.card.style.height = PACK_PROPS.height + "vmin"
-        this.card.style.width = PACK_PROPS.width + "vh"
-        this.card.style.border = "3px solid orange"
-        this.card.style.position = "absolute"
-        this.card.style.left = PACK_PROPS.left + "vmin"
+        this.card.style.width = PACK_PROPS.width + "vmin"
+        this.card.style.left = PACK_PROPS.left + "vW"
         this.card.style.top = PACK_PROPS.top + "vmin" 
+        this.card.style.fontSize = "12px"
+        this.player = player
     }
 
     calculateLeft(i){
+        const currentHand = hand[player]
         let left = (TABLE_PROPS.width/2)-(PACK_PROPS.width/2)
-        let skew = 0 + (i+(hand.length%2/2)-hand.length/2)*PACK_PROPS.width/2    
-        hand.length%2
+        let skew = 0 + (i+(currentHand.length%2/2)-currentHand.length/2)*PACK_PROPS.width/1.5    
+        currentHand.length%2
         ? {} 
         : skew += PACK_PROPS.width/4
-        return (left+skew)+"vmin"
+        return (left+skew)+"vw"
     }
 
     calculateRotation(i){
+        const currentHand = hand[player]
         let rotation
-        if (hand.length%2 == 1){
-            rotation = 0 + (i-Math.floor(hand.length / 2))*15
-        } else if (i<hand.length/2){
-            rotation = (i-hand.length/2)*15
+        const rotationRate = 15/(currentHand.length/3)
+        if (currentHand.length%2 == 1){
+            rotation = 0 + (i-Math.floor(currentHand.length / 2))* rotationRate
+        } else if (i<currentHand.length/2){
+            rotation = (i-currentHand.length/2)* rotationRate
         } else {
-            rotation = 15+(i-hand.length/2)*15
+            rotation = rotationRate +(i-currentHand.length/2)* rotationRate
+        }
+
+        if (!player){
+            rotation = -rotation + 180
         }
         return "rotate("+rotation+"deg)"
     }
@@ -38,10 +45,24 @@ export default class Card {
         container.append(this.card)                          
     }
 
+    calculateTop(){
+        if(player){
+            return "60vmin"
+        } else {
+            return "10vmin"
+        }
+    }
+
     move(currentCard, i){    
         this.card.style.left = this.calculateLeft(i)
-        this.card.style.top = "60vmin"
+        this.card.style.top = this.calculateTop()
         this.card.style.transform = this.calculateRotation(i)
+    }
+
+    play(){
+        this.card.style.left = TABLE_PROPS.width*0.45 + "vw"
+        this.card.style.top = PACK_PROPS.top + "vmin"
+        
     }
 
 }
